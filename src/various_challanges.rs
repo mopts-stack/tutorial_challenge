d25339eb2791e73bad1db2663d71cb7c82329455
Navid Dezashibi <dezashibi@gmail.com>
2023-11-23 15:10:35 +0400

    convert to morse

--- src/various_challanges.rs
+++ src/various_challanges.rs
@@ -1,4 +1,4 @@
-use std::fmt::Display;
+use std::fmt::{Display, Formatter, Result};
 
 pub fn start() {
     let mut list = vec![1.0, 4.0, 5.0];
@@ -29,6 +29,12 @@ pub fn start() {
     users.sort();
     println!("sorted using .sort():      {:?}", &users);
     println!("✅ case insensitive sort test(s) passed");
+
+    let greeting = "Hello, World";
+    println!("{}", greeting);
+    let greeting_morse = greeting.to_string().to_morse_code();
+    print_morse_code(&greeting_morse);
+    println!("✅ convert to morse test(s) passed");
 }
 
 fn median(v: &mut Vec<f64>) -> Option<f64> {
@@ -68,4 +74,84 @@ fn sort_usernames<T: AsRef<str>>(usernames: &mut [T]) {
 
     // more efficient way
     usernames.sort_by_cached_key(|x: &T| x.as_ref().to_lowercase())
+}
+
+#[derive(Debug, PartialEq)]
+enum Pulse {
+    Short,
+    Long,
+}
+type Letter = Vec<Pulse>;
+type Message = Vec<Letter>;
+trait MorseCode {
+    fn to_morse_code(&self) -> Message;
+}
+impl MorseCode for String {
+    fn to_morse_code(&self) -> Message {
+        use Pulse::*;
+
+        let mut message = Vec::with_capacity(self.len());
+        for c in self.chars() {
+            let morse = match c {
+                'A' | 'a' => vec![Short, Long],
+                'B' | 'b' => vec![Long, Short, Short, Short],
+                'C' | 'c' => vec![Long, Short, Long, Short],
+                'D' | 'd' => vec![Long, Short, Short],
+                'E' | 'e' => vec![Short],
+                'F' | 'f' => vec![Short, Short, Long, Short],
+                'G' | 'g' => vec![Long, Long, Short],
+                'H' | 'h' => vec![Short, Short, Short, Short],
+                'I' | 'i' => vec![Short, Short],
+                'J' | 'j' => vec![Short, Long, Long, Long],
+                'K' | 'k' => vec![Long, Short, Long],
+                'L' | 'l' => vec![Short, Long, Short, Short],
+                'M' | 'm' => vec![Long, Long],
+                'N' | 'n' => vec![Long, Short],
+                'O' | 'o' => vec![Long, Long, Long],
+                'P' | 'p' => vec![Short, Long, Long, Short],
+                'Q' | 'q' => vec![Long, Long, Short, Long],
+                'R' | 'r' => vec![Short, Long, Short],
+                'S' | 's' => vec![Short, Short, Short],
+                'T' | 't' => vec![Long],
+                'U' | 'u' => vec![Short, Short, Long],
+                'V' | 'v' => vec![Short, Short, Short, Long],
+                'W' | 'w' => vec![Short, Long, Long],
+                'X' | 'x' => vec![Long, Short, Short, Long],
+                'Y' | 'y' => vec![Long, Short, Long, Long],
+                'Z' | 'z' => vec![Long, Long, Short, Short],
+
+                '1' => vec![Short, Long, Long, Long, Long],
+                '2' => vec![Short, Short, Long, Long, Long],
+                '3' => vec![Short, Short, Short, Long, Long],
+                '4' => vec![Short, Short, Short, Short, Long],
+                '5' => vec![Short, Short, Short, Short, Short],
+                '6' => vec![Long, Short, Short, Short, Short],
+                '7' => vec![Long, Long, Short, Short, Short],
+                '8' => vec![Long, Long, Long, Short, Short],
+                '9' => vec![Long, Long, Long, Long, Short],
+                '0' => vec![Long, Long, Long, Long, Long],
+                _ => continue,
+            };
+            message.push(morse);
+        }
+
+        message
+    }
+}
+impl Display for Pulse {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            Pulse::Short => write!(f, "."),
+            Pulse::Long => write!(f, "_"),
+        }
+    }
+}
+fn print_morse_code(code: &Message) {
+    for letter in code.iter() {
+        for pulse in letter.iter() {
+            print!("{}", pulse);
+        }
+        print!(" ");
+    }
+    println!();
 }
